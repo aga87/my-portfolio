d0a1fff93716ab2d76507b4b43a2b1dd7d8c89f2
Aga <aga.labonarska@outlook.com>
2020-03-25 13:51:44 +0000

    merged ProjectInfo with Project

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 function Project(props) {
-  const { name, imgSrc, altName, info } = props;
+  const { name, imgSrc, altName, date, tags, links, children } = props;
+
+  function getClassName(category) {
+    switch (category) {
+      case 'html':
+        return 'u-border-html';
+      case 'css':
+        return 'u-border-css';
+      case 'js':
+        return 'u-border-js';
+      case 'redux':
+        return 'u-border-redux';
+      case 'react':
+        return 'u-border-react';
+      default:
+        return '';
+    }
+  }
+
+  const tagListItems = tags.map((tag) => {
+    const className = getClassName(tag.category.toLocaleLowerCase());
+    return <li className={`c-project-tags__item ${className}`}>{tag.tag}</li>;
+  });
+
+  const projectLinkListItems = links.map((link) => <li>{link}</li>);
 
   return (
     <div className="c-project">
@@ -14,7 +38,40 @@ function Project(props) {
           alt={`Screenshot of ${altName} on MacBook and iPhone 8`}
         />
 
-        <div>{info}</div>
+        {/* <div>{info}</div> */}
+
+        <table className="c-project-info">
+          <tr>
+            <th className="c-project-info__heading">Completion date:</th>
+            <td className="c-project-info__text">{date}</td>
+          </tr>
+          <tr>
+            <th className="c-project-info__heading">Project description:</th>
+            <td className="c-project-info__text c-project-info__text--justify">
+              {children}
+            </td>
+          </tr>
+
+          <tr>
+            <th className="c-project-info__heading">Tags:</th>
+            <td className="c-project-info__text">
+              {/* {tags} */}
+              {/* fixme: classname */}
+              <ul className="c-project-tags s1">{tagListItems}</ul>
+            </td>
+          </tr>
+
+          <tr>
+            <th rowSpan="2" className="c-project-info__heading">
+              Code:
+            </th>
+            <td className="c-project-info__text">
+              {/* fixme: classname? */}
+              <ul className="l-project-link-list">{projectLinkListItems}</ul>
+              {/* {links} */}
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
   );
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Project from './Project';
-import ProjectInfo from './ProjectInfo';
 import ProjectLinkGitHub from './ProjectLinkGitHub';
 
 import astromanMockup from '../img/smartmockups_astroman.png';
@@ -21,57 +20,52 @@ function Projects() {
               name="Astroman - Game For Two Players"
               imgSrc={astromanMockup}
               altName="the Astroman game"
-              info={
-                <ProjectInfo
-                  date="February 2020"
-                  tags={[
-                    { tag: 'CSS Grid', category: 'CSS' },
-                    { tag: 'Canvas API', category: 'other' },
-                    { tag: 'SVG', category: 'other' },
-                    { tag: 'Redux architecture', category: 'Redux' },
-                    { tag: 'Functional programming', category: 'JS' },
-                    { tag: 'ES6', category: 'JS' },
-                    { tag: 'SCSS', category: 'CSS' },
-                    { tag: 'Responsive', category: 'CSS' },
-                  ]}
-                  links={[
-                    <ProjectLinkGitHub
-                      name="See Astroman (Redux) on GitHub"
-                      href="www.google.com"
-                    />,
-                    <ProjectLinkGitHub
-                      name="See Astroman (Redux, React) on GitHub"
-                      href="www.google.com"
-                    />,
-                  ]}
-                >
-                  <p>
-                    In this hangman-inspired game, set in space, two players
-                    compete to uncover the concealed title of a randomly
-                    selected science-fiction book. The players take turns to
-                    guess letters: if the book title contains the letter, the
-                    player can go again. If the guess is incorrect, the player
-                    loses one of their 8 lives (oxygen) and their turn. The
-                    player that uncovered the last letter in the book title
-                    gains one point.
-                  </p>
-                  <p>
-                    The game consists of unlimited number of rounds and ends
-                    when one of the players runs out of oxygen. Player with
-                    higher score wins. If it&apos;s a tie, the player with
-                    lives/ oxygen left wins.
-                  </p>
-                  <p>
-                    Players can restart, go back to or quit the game from the
-                    Options modal.
-                  </p>
-                  <p>
-                    I use Redux to manage the game&apos;s state, including the
-                    UI and audio.
-                  </p>
-                </ProjectInfo>
-              }
-            />
+              date="February 2020"
+              tags={[
+                { tag: 'CSS Grid', category: 'CSS' },
+                { tag: 'Canvas API', category: 'other' },
+                { tag: 'SVG', category: 'other' },
+                { tag: 'Redux architecture', category: 'Redux' },
+                { tag: 'Functional programming', category: 'JS' },
+                { tag: 'ES6', category: 'JS' },
+                { tag: 'SCSS', category: 'CSS' },
+                { tag: 'Responsive', category: 'CSS' },
+              ]}
+              links={[
+                <ProjectLinkGitHub
+                  name="See Astroman (Redux) on GitHub"
+                  href="www.google.com"
+                />,
+                <ProjectLinkGitHub
+                  name="See Astroman (Redux, React) on GitHub"
+                  href="www.google.com"
+                />,
+              ]}
+            >
+              <p>
+                In this hangman-inspired game, set in space, two players compete
+                to uncover the concealed title of a randomly selected
+                science-fiction book. The players take turns to guess letters:
+                if the book title contains the letter, the player can go again.
+                If the guess is incorrect, the player loses one of their 8 lives
+                (oxygen) and their turn. The player that uncovered the last
+                letter in the book title gains one point.
+              </p>
+              <p>
+                The game consists of unlimited number of rounds and ends when
+                one of the players runs out of oxygen. Player with higher score
+                wins. If it&apos;s a tie, the player with lives/ oxygen left
+                wins.
+              </p>
+              <p>
+                Players can restart, go back to or quit the game from the
+                Options modal.
+              </p>
+              <p>
+                I use Redux to manage the game&apos;s state, including the UI
+                and audio.
+              </p>
+            </Project>
           </li>
         </ol>
       </section>
